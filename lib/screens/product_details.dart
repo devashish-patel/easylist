60f6b0551a8a85865916220f91630e1b74abf2c4
Devashish Patel <devashish@criticalstart.com>
2019-02-07 00:43:09 -0600

    fix: reactions upon back button pressing on product details

--- lib/screens/product_details.dart
+++ lib/screens/product_details.dart
@@ -1,5 +1,6 @@
 import 'package:easylist/models/product.dart';
 import 'package:flutter/material.dart';
+import 'dart:async';
 
 class ProductDetails extends StatelessWidget {
   final Product product;
@@ -7,42 +8,51 @@ class ProductDetails extends StatelessWidget {
   ProductDetails(this.product);
   Widget get navBarTitle => Text(product.name);
 
+  Widget productDetails(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.end,
+      mainAxisAlignment: MainAxisAlignment.start,
+      children: <Widget>[
+        RaisedButton(
+          color: Theme.of(context).accentColor,
+          child: Text('Delete'),
+          onPressed: () {
+            return Navigator.pop(context, true);
+          },
+        ),
+        Image.asset(
+          product.imageLink,
+          height: 400.0,
+          width: 600.0,
+        ),
+        Container(
+          padding: EdgeInsets.all(5.0),
+          child: Center(
+            child: Text(product.name),
+          ),
+        ),
+        Container(
+          padding: EdgeInsets.all(5.0),
+          child: Center(
+            child: Text('${product.price}'),
+          ),
+        )
+      ],
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: navBarTitle,
-      ),
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.end,
-        mainAxisAlignment: MainAxisAlignment.start,
-        children: <Widget>[
-          RaisedButton(
-            color: Theme.of(context).accentColor,
-            child: Text('Delete'),
-            onPressed: () {
-              return Navigator.pop(context, true);
-            },
+    return WillPopScope(
+        child: Scaffold(
+          appBar: AppBar(
+            title: navBarTitle,
           ),
-          Image.asset(
-            product.imageLink,
-            height: 400.0,
-            width: 600.0,
-          ),
-          Container(
-            padding: EdgeInsets.all(5.0),
-            child: Center(
-              child: Text(product.name),
-            ),
-          ),
-          Container(
-            padding: EdgeInsets.all(5.0),
-            child: Center(
-              child: Text('${product.price}'),
-            ),
-          )
-        ],
-      ),
-    );
+          body: productDetails(context),
+        ),
+        onWillPop: () {
+          Navigator.pop(context, false);
+          return Future.value(false);
+        });
   }
 }
